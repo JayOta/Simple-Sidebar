1eb446dc42ed64ee34942a2d764c197b2368fce6
JayOta <otavio.cvilela@gmail.com>
2024-09-18 16:08:42 -0300

    <3

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function closeSidebar(){
-    document.getElementById('sidebar').style.display = 'none';
-}
-function openSidebar(){
-    document.getElementById('sidebar').style.display = 'block';
-    document.getElementById('sidebar').style.transition = '.5s ease-in';
-}
+document
+  .getElementById("openSidebarBtn")
+  .addEventListener("click", function (e) {
+    document.getElementById("sidebar").style.display = "block";
+    document.getElementById("openSidebarBtn").style.display = "none";
+    e.preventDefault();
+  });
+
+document
+  .getElementById("closeSidebarBtn")
+  .addEventListener("click", function (e) {
+    document.getElementById("sidebar").style.display = "none";
+    document.getElementById("openSidebarBtn").style.display = "block";
+    e.preventDefault();
+  });
